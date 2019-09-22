63434408fc3366ee81c6b65b8041572110933f44
Daniel Oshiro <daniel0oshiro@gmail.com>
2019-09-22 17:17:39 -0300

    Fix input acessibility bug

--- src/components/AddInput.vue
+++ src/components/AddInput.vue
@@ -5,7 +5,7 @@
     @submit.prevent="validateForm"
     @keydown.enter="validateForm"
   >
-    <label class="hidden" for="add">Add New Todo</label>
+    <label class="sr-only" for="add">Add New Todo</label>
 
     <input
       v-model="todoText"
@@ -54,3 +54,18 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.sr-only,
+.sr-only-focusable:not(:focus) {
+  position: absolute !important;
+  width: 1px !important;
+  height: 1px !important;
+  padding: 0 !important;
+  margin: -1px !important;
+  overflow: hidden !important;
+  clip: rect(0, 0, 0, 0) !important;
+  white-space: nowrap !important;
+  border: 0 !important;
+}
+</style>
