3fc8451d159370d16c9e146a9891f1db18037347
Daniel Oshiro <daniel0oshiro@gmail.com>
2019-09-22 08:25:47 -0300

    List component

---
+++ src/components/List.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <ListItem />
+    <ListItem />
+    <ListItem />
+    <ListItem />
+    <ListItem />
+    <ListItem />
+
+  </section>
+</template>
+
+<script>
+import ListItem from '@/components/ListItem'
+
+export default {
+  name: 'List',
+  components: {
+    ListItem
+  }
+}
+</script>
