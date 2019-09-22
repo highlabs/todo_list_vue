1ab476c7170481ffdf49258c6803e9f37840e491
Daniel Oshiro <daniel0oshiro@gmail.com>
2019-09-22 15:11:08 -0300

    Store mutations refactor

--- tests/unit/store.spec.js
+++ tests/unit/store.spec.js
@@ -1,53 +1,32 @@
-import { mutations } from '@/store'
+import { mutations, state } from '@/store'
 
 describe('Vuex', () => {
   describe('Mutations', () => {
-    let todoList
-    let state
-
-    beforeEach(() => {
-      todoList = [
-        {
-          id: '1',
-          label: 'Make dinner',
-          completed: true
-        },
-        {
-          id: '2',
-          label: 'Do the dishes',
-          completed: true
-        },
-        {
-          id: '3',
-          label: 'write a post',
-          completed: true
-        }
-      ]
-
-      state = {
-        todoList: todoList,
-        darkMode: false
+    const todoList = [
+      {
+        id: '1',
+        label: 'Make dinner',
+        completed: true
+      },
+      {
+        id: '2',
+        label: 'Do the dishes',
+        completed: true
+      },
+      {
+        id: '3',
+        label: 'write a post',
+        completed: true
       }
-    })
+    ]
     it('add todo to the list when commit AddTodo', () => {
-      const itemToAdd = {
-        id: 4,
-        label: 'Item to add in todo list',
-        completed: false
-      }
-      mutations.addTodo(state, itemToAdd)
-      expect(state.todoList[0]).toEqual(todoList[0])
-      expect(state.todoList[3]).toEqual(itemToAdd)
-    })
-
-    it('remove todo from the list when commit RemoveTodo', () => {
-      const itemToRemove = todoList[1]
-
-      mutations.removeTodo(state, itemToRemove)
+      mutations.addTodo(state, todoList[0])
+      mutations.addTodo(state, todoList[1])
+      mutations.addTodo(state, todoList[2])
 
       expect(state.todoList[0]).toEqual(todoList[0])
-      expect(state.todoList[1]).toEqual(todoList[2])
-      expect(state.todoList[2]).toBe(undefined)
+      expect(state.todoList[1]).toEqual(todoList[1])
+      expect(state.todoList[2]).toEqual(todoList[2])
     })
 
     it('toggle Todo when commit toggleTodo', () => {
@@ -60,10 +39,32 @@ describe('Vuex', () => {
       expect(state.todoList[2].completed).toBe(true)
     })
 
+    it('remove todo from the list when commit RemoveTodo', () => {
+      const itemToRemove = todoList[1]
+
+      mutations.removeTodo(state, itemToRemove)
+
+      expect(state.todoList[0]).toEqual(todoList[0])
+      expect(state.todoList[1]).toEqual(todoList[2])
+      expect(state.todoList[2]).toBe(undefined)
+    })
+
     it('toggle darkMode when commit toggleDarkMode', () => {
       mutations.toggleDarkMode(state, !state.darkMode)
 
       expect(state.darkMode).toBe(true)
+    })
+
+    it('toggle showDone when commit toggleDone', () => {
+      mutations.toggleDarkMode(state, !state.showDone)
+
+      expect(state.showDone).toBe(true)
+    })
+
+    it('toggle showTrash when commit toggleTrash', () => {
+      mutations.toggleTrash(state, !state.showTrash)
+
+      expect(state.showTrash).toBe(true)
     })
   })
 })
