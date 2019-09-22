5212aa8f33f6893230bdfd83197dee9dcad2de38
Daniel Oshiro <daniel0oshiro@gmail.com>
2019-09-22 08:24:46 -0300

    ListItem component

---
+++ src/components/ListItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="py-4">
+    <Checkbox title="lorem  ipsum"/>
+  </div>
+</template>
+
+<script>
+import Checkbox from '@/components/Checkbox'
+export default {
+  name: 'ListItem',
+  components: {
+    Checkbox
+  }
+}
+</script>
