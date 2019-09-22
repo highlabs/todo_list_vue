f6b56244df4a5e9a69844e01538741fd59c123b4
Daniel Oshiro <daniel0oshiro@gmail.com>
2019-09-22 15:02:38 -0300

    Menu options

---
+++ src/components/Menu.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex justify-between md:justify-end py-2 border-b border-t border-gray-200">
+    <button ref="darkModeButton" class="px-2 py-1 text-sm" @click="toggleDarkMode">
+      Modo Noturno
+    </button>
+    <button ref="doneButton" class="px-2 py-1 text-sm" @click="toggleDone">
+      Esconder concluídos
+    </button>
+    <button ref="archiveButton" class="px-2 py-1 text-sm" @click="toggleTrash">
+      Mostrar arquivados
+    </button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'Menu',
+  methods: {
+    ...mapActions(['toggleDarkMode', 'toggleDone', 'toggleTrash'])
+  }
+}
+</script>
---
+++ tests/unit/menu.spec.js
@@ -0,0 +1,56 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import Menu from '@/components/Menu.vue'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('Menu.vue', () => {
+  describe('> Actions', () => {
+    let actions
+    let store
+    let state
+
+    beforeEach(() => {
+      state = {
+        darkMode: false
+      }
+      actions = {
+        toggleDarkMode: jest.fn(),
+        toggleDone: jest.fn(),
+        toggleTrash: jest.fn()
+      }
+      store = new Vuex.Store({
+        state,
+        actions
+      })
+    })
+
+    it('dispatches "toggleDarkMode" button is clicked', () => {
+      const wrapper = shallowMount(Menu, { store, localVue })
+
+      const button = wrapper.find({ ref: 'darkModeButton' })
+      button.trigger('click')
+
+      expect(actions.toggleDarkMode).toHaveBeenCalled()
+    })
+
+    it('dispatches "toggleDone" button is clicked', () => {
+      const wrapper = shallowMount(Menu, { store, localVue })
+
+      const button = wrapper.find({ ref: 'doneButton' })
+      button.trigger('click')
+
+      expect(actions.toggleDone).toHaveBeenCalled()
+    })
+
+    it('dispatches "toggleArchived" button is clicked', () => {
+      const wrapper = shallowMount(Menu, { store, localVue })
+
+      const button = wrapper.find({ ref: 'archiveButton' })
+      button.trigger('click')
+
+      expect(actions.toggleTrash).toHaveBeenCalled()
+    })
+  })
+})
