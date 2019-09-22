2350e7348c3f7aa03b9ec34e6170403295a66878
Daniel Oshiro <daniel0oshiro@gmail.com>
2019-09-22 08:25:58 -0300

    Add list to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app" class="container mx-auto px-6 font-body">
     <Header />
+    <List />
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header'
+import List from '@/components/List'
 
 export default {
   name: 'app',
   components: {
-    Header
+    Header,
+    List
   }
 }
 </script>
