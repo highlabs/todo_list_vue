3d37c60df94344820e8cfb66521822c9fcf9500a
Daniel Oshiro <daniel0oshiro@gmail.com>
2019-09-22 13:12:52 -0300

    Add list to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="container mx-auto px-6 font-body">
     <Header />
-    <List />
+    <List :list="list" />
   </div>
 </template>
 
@@ -14,6 +14,11 @@ export default {
   components: {
     Header,
     List
+  },
+  computed: {
+    list () {
+      return this.$store.state.todoList
+    }
   }
 }
 </script>
